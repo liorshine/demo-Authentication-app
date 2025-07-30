2f0005b2b92626231881bed29b5327d0b3b5542c
trangngnnhat <trangngnnhat@gmail.com>
2025-07-30 13:22:14 +0700

    signup and login

---
+++ src/models/userModel.js
@@ -0,0 +1,36 @@
+import { verify } from "crypto";
+import mongoose, { mongo } from "mongoose"
+import { type } from "os";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide a email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    isVerfied: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
+
+const User = mongoose.models.users || mongoose.model("User", userSchema);
+
+export default User;
